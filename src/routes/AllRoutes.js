4e17c1a011d53485551e65bbe469542db01de0e6
sandhanaK2002 <sandhanakrishnan010@gmail.com>
2023-04-27 18:51:05 +0530

    updated IN_stock & Protected Routes

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -1,8 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import { HomePage, ProductsList, ProductDetail ,CartPage } from "../pages";
+import {ProtectedRoutes} from "./ProtectedRoutes"
 import {Login , Register} from "../pages"
 
 export const AllRoutes = () => {
+
+
   return (
     <>
     <Routes>
@@ -11,7 +14,9 @@ export const AllRoutes = () => {
         <Route path="products/:id" element={<ProductDetail />} />
         <Route path = "login"  element={<Login/>}/>
         <Route path = "register" element ={<Register/>} />
-        <Route path = "cart"  element={<CartPage/>}/>
+
+
+        <Route path = "cart"  element={ <ProtectedRoutes> <CartPage/> </ProtectedRoutes>} />  
     </Routes>
     </>
   )
