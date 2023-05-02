e2d564f4d8c2dfc100bca863cfd9db5eb4c7b049
sandhanaK2002 <sandhanakrishnan010@gmail.com>
2023-05-02 13:24:05 +0530

    Added Page Not Found Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ export { CartPage } from "./Cart/CartPage";
 
 export {OrderPage} from "./Order/OrderPage"
 export {DashboardPage} from "./Dashboard/DashboardPage"
+export {PageNotFound} from "./PageNotFound"
 
 export { Login } from "./Login";
 export { Register } from "./Register";
